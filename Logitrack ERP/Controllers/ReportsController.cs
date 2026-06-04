ff59cd5035018c37cbab990269436d98cff7a3f9
f2023065254-sudo <f2023065254@umt.edu.pk>
2026-06-04 21:56:37 +0500

    updated

---
+++ Logitrack ERP/Controllers/ReportsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logitrack_ERP.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // Reports & Analytics Dashboard Main Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Logitrack ERP/Controllers/RiskController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logitrack_ERP.Controllers
+{
+    [Route("Complaints")] // <--- Yeh line poore controller ka URL /Complaints par map kar degi
+    public class RiskController : Controller
+    {
+        [HttpGet("")] // Default route yani /Complaints
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("RiskReports")] // Route banega: /Complaints/RiskReports
+        public IActionResult RiskReports()
+        {
+            return View();
+        }
+
+        [HttpGet("Create")] // Route banega: /Complaints/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet("Edit")] // Route banega: /Complaints/Edit
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpGet("Details")] // Route banega: /Complaints/Details
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
